c9c4a3e8814c1f775306f11c59757e0e9eaeffe3
Vedant Patil <79906441+vedantpatil2021@users.noreply.github.com>
2025-02-04 22:08:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,68 +37,11 @@ chmod +x install.sh
 bash /home/icicle/install.sh
 
 # 6) Make the directoy named ea2openmobile
-mkdir -p ./icicleEdge/ea2openmobile
+mkdir -p /home/icicle/icicleEdge/ea2openmobile
 
 # 7) Download restartMicroservice.sh
-RSTMCS="/home/icicle/icicleEdge/ea2openmobile/restartMicroservice.sh"
-
-sudo touch $RSTMCS
-chmod +x $RSTMCS
-
-sudo tee $NGINX_CONF > /dev/null <<EOF
-#!/bin/bash
-
-echo "Shutting down local instances to get up-to-date IP address"
-echo "Killall and Uninstall k3s"
-sudo bash /usr/local/bin/k3s-killall.sh
-sudo bash /usr/local/bin/k3s-uninstall.sh
-sudo rm /var/lib/rancher/k3s/server/token
-sudo rm -rf /etc/ceph \
-       /etc/cni \
-       /etc/kubernetes \
-       /etc/rancher \
-       /opt/cni \
-       /opt/rke \
-       /run/secrets/kubernetes.io \
-       /run/calico \
-       /run/flannel \
-       /var/lib/calico \
-       /var/lib/etcd \
-       /var/lib/cni \
-       /var/lib/kubelet \
-       /var/lib/rancher\
-       /var/log/containers \
-       /var/log/kube-audit \
-       /var/log/pods \
-       /var/run/calico
-
-
-
-sleep 3
-bash /home/icicle/icicleEdge/adminTools/edgeTools/setupOfflineMode.sh reset
-bash /home/icicle/icicleEdge/adminTools/edgeTools/setupOfflineMode.sh init
-
-# Do a fresh install
-echo "Installing K3s -- sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"server --flannel-iface=icl43\" sh -"
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-iface=icl43" /bin/sh -
-sleep 5
-
-
-NODE_NAME=`sudo k3s kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml get nodes| tail -n 1 | awk '{print $1}' `
-EDGE_ID=$NODE_NAME
-EDGE_ID+="-edgedevel"
-sudo k3s kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml label node $NODE_NAME icicletype=edgedevel
-echo $EDGE_ID > ~/.ssh/icicletype
-
-
-cd /home/icicle/icicleEdge
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 30080website
-echo Deployed Website!
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 4242phonehub
-echo Deployed Phone Hub!  
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 1212aimissions 
-echo Deployed AI Mission in the background!
-EOF
+curl -L -o /home/icicle/icicleEdge/ea2openmobile/restartMicroservice.sh https://raw.githubusercontent.com/icicle-openpass/Openmobile-Setup/refs/heads/main/restartMicroservice.sh
+chmod +x /home/icicle/icicleEdge/ea2openmobile/restartMicroservice.sh
 
 # 8) Install NGINX
 sudo apt install -y nginx
@@ -106,7 +49,7 @@ sudo apt install -y nginx
 # 9) Edit default file
 NGINX_CONF="/etc/nginx/sites-available/default"
 
-sudo cp $NGINX_CONF "$NGINX_CONF.bak"
+sudo rm -rf $NGINX_CONF
 
 sudo tee $NGINX_CONF > /dev/null <<EOF
 server {
@@ -149,16 +92,7 @@ EOF
 sudo nginx -t
 
 # 11) Restart the NGINX service
-if [ $? -eq 0 ]; then
-    echo "Nginx configuration is valid. Restarting Nginx..."
-    sudo systemctl restart nginx
-    echo "Nginx restarted successfully!"
-else
-    echo "Nginx configuration failed. Restoring backup..."
-    sudo cp "$NGINX_CONF.bak" $NGINX_CONF
-    sudo nginx -t
-    exit 1
-fi
+sudo systemctl restart nginx
 
 # 12) Run the restartMicroservice file.
 bash /home/icicle/icicleEdge/ea2openmobile/restartMicroservice.sh
