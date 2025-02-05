d454f6aba0cba5790cb68607f242e809136ebf5f
Vedant Patil <79906441+vedantpatil2021@users.noreply.github.com>
2025-02-04 22:30:14 -0500

    Update restartMicroservice.sh

--- restartMicroservice.sh
+++ restartMicroservice.sh
@@ -44,9 +44,9 @@ echo $EDGE_ID > ~/.ssh/icicletype
 
 
 cd /home/icicle/icicleEdge
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 30080website
+/home/icicle/icicleEdge/bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 30080website
 echo Deployed Website!
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 4242phonehub
+/home/icicle/icicleEdge/bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 4242phonehub
 echo Deployed Phone Hub!  
-./bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 1212aimissions 
+/home/icicle/icicleEdge/bin/deployMicroservice.py -home `pwd` -devel -edge edgedevel 1212aimissions 
 echo Deployed AI Mission in the background!
